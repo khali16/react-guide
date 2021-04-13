6ec92f07620a29add991e35f0dd34eaa21a7e7a7
khali16 <linertk@gmail.com>
2021-04-13 09:53:52 +0200

    Doin something

---
+++ src/components/Expense Item/ExpenseItem.js
@@ -0,0 +1,16 @@
+import styles from "./ExpenseItem.module.css";
+import ExpenseDate from "../expenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <div className={styles.ExpenseItem}>
+      <div className={styles.ExpenseItemDescription}>
+        <ExpenseDate date={props.date} />
+        <h2>{props.title}</h2>
+        <div className={styles.ExpenseItemPrice}>{props.amount}$</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/expenseDate.js
@@ -0,0 +1,16 @@
+import styles from "./ExpenseDate.module.css";
+
+function ExpenseDate(props) {
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  return (
+    <div className={styles.ExpenseDate}>
+      <div className={styles.ExpenseDateDay}>{day}</div>
+      <div className={styles.ExpenseDateMonth}>{month}</div>
+      <div className={styles.ExpenseDateYear}>{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
